6f372a8add6efef2f5a520f9b70f15491ae2d234
PiyushMishra15 <piyushmishra2915@gmail.com>
2025-06-03 22:38:23 +0530

    D

--- src/pages/Account/PrivateRoute.jsx
+++ src/pages/Account/PrivateRoute.jsx
@@ -1,30 +1,11 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const token = localStorage.getItem("token");
 
-  const baseURL = "https://cropifybackend.onrender.com/api"; // Change if needed
-  const navigate = useNavigate();
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
 
-  useEffect(() => {
-    console.log("Checking auth status...");
-    axios
-      .get(`${baseURL}/auth/verifyToken`, { withCredentials: true })
-      .then((res) => {
-        setIsAuthenticated(true);
-      })
-      .catch((err) => {
-        if (err.response?.status === 401) {
-          navigate("/login");
-        } else {
-          navigate("/login");
-        }
-        setIsAuthenticated(false);
-      });
-  }, [navigate]);
-
-  if (isAuthenticated === null) return <div>Loading...</div>;
-  return isAuthenticated ? children : null;
+  return children;
 }
